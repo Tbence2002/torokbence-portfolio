46e912ff953cc145449b5d6bae79d8cd56daa6c7
TBence2002 <torokbencee.2002@gmail.com>
2023-12-06 23:34:58 +0100

    Kapcsolat

--- src/components/Kapcsolat.jsx
+++ src/components/Kapcsolat.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import "./kapcsolat.css"
 import { FaFacebookSquare, FaLinkedin } from "react-icons/fa";
+import Lottie, {LottieComponentProps} from 'lottie-react';
+import animationData from '../assets/kapcsolat.json'
 
 function Kapcsolat() {
+    const emailRef = useRef<LottieComponentProps>(null);
     return (
         <div className="kapcsolat-container">
             <div className="kapcsolat-content">
@@ -27,19 +30,22 @@ function Kapcsolat() {
                             <p>Telefon</p>
                             <p>+36203758874</p>
                         </div>
+                        <div className="email-anim">
+                            <Lottie lottieRef={emailRef} Loop={false} onComplete={()=> {emailRef.current?.goToAndPlay(45,true)}} animationData={animationData}/>
+                        </div>
                     </div>
                     <div className="kapcsolat-right">
                         <div className="kapcsolat-message">
                             <input type="text" name="teljes_nev" required/>
-                            <label>Teljes neve</label>
+                            <label for="teljes_nev">Teljes neve</label>
                         </div>
                         <div className="kapcsolat-message">
-                            <input type="email" name="email" required/>
-                            <label>Email címe</label>
+                            <input type="text" name="email" required/>
+                            <label for="email">Email címe</label>
                         </div>
                         <div className="kapcsolat-message">
                             <textarea name="uzenet" required></textarea>
-                            <label>Üzenet</label>
+                            <label for="uzenet">Üzenet</label>
                         </div>
                         <div className="kapcsolat-button">
                             <button>Küldés</button>
