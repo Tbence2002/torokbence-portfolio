e552291204b2396a56a1522b083983480a08ed13
TBence2002 <torokbencee.2002@gmail.com>
2023-12-08 23:45:11 +0100

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,14 @@
 import React,{useRef} from 'react'
-import Lottie, {LottieCurrentProps} from 'lottie-react'
-import animationData from '../assets/footer.json'
+
 import "./footer.css"
 
 function Footer() {
-    const footerRef = useRef<LottieCurrentProps>(null);
+  
   return (
     <div className='footer-container'>
         <div className="footer-content">
             <div className="footer-image">
-                <Lottie onComplete={()=> {footerRef.current?.goToAndPlay(45, true)}} Loop={false} lottieRef={footerRef} animationData={animationData}/>
+               
                 <div className="footer-text">
                 <h3>Török Bence, Junior Frontend Fejlesztő.</h3>
             </div>
