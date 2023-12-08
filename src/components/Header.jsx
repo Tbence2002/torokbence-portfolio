01684373c7f97872af834f5ab0197e7127e06e20
TBence2002 <torokbencee.2002@gmail.com>
2023-12-08 23:45:38 +0100

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,21 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import "./header.css"
-import {gsap} from 'gsap'
+import { IoMdClose } from "react-icons/io";
+import { HiOutlineMenuAlt2 } from "react-icons/hi";
 
 function Header() {
-  useEffect(()=> {
-    gsap.set('.header', {y:'-100px'});
-    gsap.set('.rolam, .munkaim, .kapcsolat', {y:'-100px'});
-    gsap.to('.header', {
-      duration:2,
-      y:'0px',
-      ease:'power2.inOut',
-      delay:2.5,
-    })
-    gsap.to('.rolam', {
-      duration:2,
-      y:'0px',
-      ease:'power2.inOut',
-      delay:2.2,
-    })
-    gsap.to('.munkaim', {
-      duration:2,
-      y:'0px',
-      ease:'power2.inOut',
-      delay:2.4,
-    })
-    gsap.to('.kapcsolat', {
-      duration:2,
-      y:'0px',
-      ease:'power2.inOut',
-      delay:2.5,
-    })
-  })
+  const [active, setActive] = useState(false);
+
   return (
     <div className='header-container'>
         <header className='header'>
-          <ul>
+          <div className="menu-open" onClick={()=> setActive(!active)}>
+            <HiOutlineMenuAlt2/>
+          </div>
+          <ul className={active ? "show-nav" : ""}>
+            <div className="menu-close" onClick={()=> setActive(!active)}>
+              <IoMdClose/>
+            </div>
             <li className='rolam'><a href="#rolam">Rólam</a></li>
             <li className='munkaim'><a href="#munkaim">Munkáim</a></li>
             <li className='kapcsolat'><a href="#kapcsolat">Kapcsolat</a></li>
