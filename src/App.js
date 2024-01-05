8842efdce251e7ac98e62025df7bf640b7721c81
TBence2002 <torokbencee.2002@gmail.com>
2024-01-05 21:15:32 +0100

    App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import './App.css';
+import React, {useState} from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Rolam from './components/Rolam';
 import Munkaim from './components/Munkaim';
 import Kapcsolat from './components/Kapcsolat';
 import Footer from './components/Footer';
-import 'react-vertical-timeline-component/style.min.css';
 import IsmerjenMeg from './components/IsmerjenMeg';
+import './App.css';
+import 'react-vertical-timeline-component/style.min.css';
 
 function App() {
+  const [next, setNext] = useState(1);
+
   return (
     <div className="home-container">
       <Header/>
       <Hero/>
       <IsmerjenMeg/>
       <Rolam/>
-      <Munkaim/>
+      <Munkaim next={next} setNext={setNext}/>
       <Kapcsolat/>
       <Footer/>
     </div>
