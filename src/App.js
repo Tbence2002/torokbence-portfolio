16f1aaba0c9ac0dcb221b286ef2935d3fabf2aad
TBence2002 <torokbencee.2002@gmail.com>
2024-01-16 00:01:59 +0100

    App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React, {useState, Suspense} from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Rolam from './components/Rolam';
+import Munkaim from './components/Munkaim';
+import Kapcsolat from './components/Kapcsolat';
+import Footer from './components/Footer';
+import IsmerjenMeg from './components/IsmerjenMeg';
 import './App.css';
 import 'react-vertical-timeline-component/style.min.css';
-
-import { ErrorBoundary } from 'react-error-boundary';
-import ErrorFallback from './components/ErrorBoundary';
-
-const Rolam = React.lazy(()=> import("./components/Rolam"));
-const IsmerjenMeg = React.lazy(()=> import("./components/IsmerjenMeg"));
-const Munkaim = React.lazy(()=> import("./components/Munkaim"));
-const Kapcsolat = React.lazy(()=> import("./components/Kapcsolat"));
-const Footer = React.lazy(()=> import("./components/Footer"));
 
 function App() {
   const [next, setNext] = useState(1);
@@ -20,35 +16,11 @@ function App() {
     <div className="home-container">
       <Header/>
       <Hero/>
-      <ErrorBoundary FallbackComponent={ErrorFallback} onReset={()=> {}}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <IsmerjenMeg/>
-        </Suspense>
-      </ErrorBoundary>
-
-      <ErrorBoundary FallbackComponent={ErrorFallback} onReset={()=> {}}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Rolam/>
-        </Suspense>
-      </ErrorBoundary>
-
-      <ErrorBoundary FallbackComponent={ErrorFallback} onReset={()=> {}}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Munkaim next={next} setNext={setNext}/>
-        </Suspense>
-      </ErrorBoundary>
-
-      <ErrorBoundary FallbackComponent={ErrorFallback} onReset={()=> {}}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Kapcsolat/>
-        </Suspense>
-      </ErrorBoundary>
-
-      <ErrorBoundary FallbackComponent={ErrorFallback} onReset={()=> {}}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Footer/>
-        </Suspense>
-      </ErrorBoundary>
+      <IsmerjenMeg/>
+      <Rolam/>
+      <Munkaim next={next} setNext={setNext}/>
+      <Kapcsolat/>
+      <Footer/>
     </div>
   );
 }
